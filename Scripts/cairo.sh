97ddc4c1715c35d406c7b3fb96b2803e4d5fde32
december0123 <december0123@users.noreply.github.com>
2013-08-18 20:17:04 +0200

    Update cairo.sh

--- Scripts/cairo.sh
+++ Scripts/cairo.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 xfconf-query --channel=xfwm4 --property=/general/use_compositing --set=false
 killall dcompmgr
+killall cairo-compmgr #in case different instance is running
 cairo-compmgr &
