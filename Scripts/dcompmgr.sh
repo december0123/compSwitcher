b9dbf609e7017c3c6b4b18d603b66c43442d9de3
december0123 <december0123@users.noreply.github.com>
2013-08-18 20:17:29 +0200

    Update dcompmgr.sh

--- Scripts/dcompmgr.sh
+++ Scripts/dcompmgr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 xfconf-query --channel=xfwm4 --property=/general/use_compositing --set=false
 killall cairo-compmgr
-killall dcompmgr
+killall dcompmgr #in case different instance is running
 dcompmgr --gl > /dev/null &
