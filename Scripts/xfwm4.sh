a3bf8a9f9dbf6de798a7f1a892ec36f50016360f
december <december0123@gmail.com>
2013-09-28 09:42:54 +0200

    update xfwm4.sh

--- Scripts/xfwm4.sh
+++ Scripts/xfwm4.sh
@@ -2,4 +2,5 @@
 killall dcompmgr
 killall cairo-compmgr
 xfconf-query --channel=xfwm4 --property=/general/use_compositing --set=false
+sleep 0.1
 xfconf-query --channel=xfwm4 --property=/general/use_compositing --set=true
